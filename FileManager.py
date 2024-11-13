ad5dbb22cdf093f498b408cbc245bc0dce6c86e7
DawZaw <dzawadzki94@gmail.com>
2024-11-13 22:40:37 +0100

    Finished version

--- FileManager.py
+++ FileManager.py
@@ -1,25 +1,21 @@
-import os
-
 class FileManager:
     path: str
 
-    
-    def __init__(self, path: str = os.path.dirname(__file__)) -> None:
-        """Domyślną ścieżką jest folder z tym plikiem."""
+    def __init__(self, path: str) -> None:
         self.path = path
-        
-    def read(self, filename:str) -> str | None:
+
+    def read(self, filename: str) -> str | None:
         filepath: str = self.path + rf"\{filename}"
-        
-        try: 
+
+        try:
             with open(filepath, "r", encoding="UTF-8") as file:
                 return file.read()
         except FileNotFoundError:
             print(f"Plik o nazwie '{filename}' nie został znaleziony.")
             return None
-        
+
     def write(self, text: str, filename: str) -> None:
         filepath: str = self.path + rf"\{filename}"
-        
+
         with open(filepath, "w", encoding="UTF-8") as file:
             file.write(text)
--- main.py
+++ main.py
@@ -1,14 +1,50 @@
+import os
+from argparse import ArgumentParser
+from typing import Any
+
+from API import API
 from FileManager import FileManager
 
-file_manager: FileManager = FileManager()
+from settings import SYSTEM_PROMPT
 
 
-def main() -> None:
-    text: str | None = file_manager.read('artykul.txt')
-    
-    if text:
-        file_manager.write(text, 'artykul.html')
+def main(filepath: str, filename: str) -> None:
+    openai_api: API = API(SYSTEM_PROMPT)
+    file_manager: FileManager = FileManager(filepath)
+
+    article: str | None = file_manager.read(filename)
+    if not article:
+        return
+
+    response: str | None = openai_api.get_response(article)
+    if not response:
+        return
+
+    file_manager.write(response, "artykul.html")
 
 
 if __name__ == "__main__":
-    main()
+    parser: ArgumentParser = ArgumentParser(
+        description="Wczytaj plik tekstowy z artykułem, aby przekonwertować go do HTML przy pomocy OpenAI.",
+    )
+    parser.add_argument(
+        "-f",
+        "--filename",
+        type=str,
+        required=True,
+        help="Nazwa pliku z artykułem.",
+    )
+    parser.add_argument(
+        "-p",
+        "--filepath",
+        type=str,
+        required=False,
+        help="Ścieżka do folderu z artykułem. Domyślnie jest to folder z programem.",
+        default=os.path.dirname(__file__),
+    )
+    args: dict[str, Any] = vars(parser.parse_args())
+
+    filepath: str = args.get("filepath")
+    filename: str = args.get("filename")
+
+    main(filepath, filename)
